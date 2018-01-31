e3f1592fb33b1fc6ed6a205a34a0f66c35249754
jesse killinger <jkillinger9@gmail.com>
2018-01-31 16:03:16 -0500

    addclient button functionality beginning

--- site/addClient.aspx.vb
+++ site/addClient.aspx.vb
@@ -2,4 +2,7 @@
 Partial Class addClient
     Inherits System.Web.UI.Page
 
+    Protected Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim fname, minit, lname, email, 
+    End Sub
 End Class
